64145ef8724c462352d8ff6842604a4cf4ab6999
BrunoItacaramby <brunobob69@hotmail.com>
2024-02-20 20:45:03 -0300

    Testando efeitos de scroll nas habilidades

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,52 @@
 const meuNome = document.querySelector('.meu-nome');
 const fName = meuNome.innerHTML;
-var button = document.getElementById('ver-mais')
+var button = document.getElementById('botao-habilidades')
 var habilidade = document.getElementById('habilidades')
 var gradiente = document.getElementById('gradiente')
 meuNome.innerHTML='';
+
+window.sr = ScrollReveal({ reset: true})
+
+sr.reveal('.sobre-mim-content', {
+    // rotate: {x: 0, y: 80, z: 0},
+    distance: '200px',
+    origin: 'left',
+    duration: 500
+});
+
+sr.reveal('.skill1', {
+    // rotate: {x: 0, y: 80, z: 0},
+    distance: '200px',
+    origin: 'left',
+    duration: 500,
+    delay: 150
+});
+
+sr.reveal('.skill2', {
+    // rotate: {x: 0, y: 80, z: 0},
+    distance: '200px',
+    origin: 'right',
+    duration: 500,
+    delay: 350
+});
+
+sr.reveal('.skill3', {
+    // rotate: {x: 0, y: 80, z: 0},
+    distance: '200px',
+    origin: 'left',
+    duration: 500,
+    delay: 550
+});
+
+sr.reveal('.skill4', {
+    // rotate: {x: 0, y: 80, z: 0},
+    distance: '200px',
+    origin: 'right',
+    duration: 500,
+    delay: 750
+});
+
+
 
 function typeWriter (nome) {
     setTimeout( () => {
