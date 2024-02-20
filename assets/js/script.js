3b0b253ef3cfbeb062e014c6811d2b03fa2a2b8e
BrunoItacaramby <brunobob69@hotmail.com>
2024-02-19 22:32:14 -0300

    Adicioando JavaScript ao botao Ver Mais e Ver Menos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 const meuNome = document.querySelector('.meu-nome');
 const fName = meuNome.innerHTML;
+var button = document.getElementById('ver-mais')
+var habilidade = document.getElementById('habilidades')
+var gradiente = document.getElementById('gradiente')
 meuNome.innerHTML='';
 
 function typeWriter (nome) {
@@ -13,6 +16,19 @@ function typeWriter (nome) {
     })}, 1500)
     
 }
+
+button.addEventListener('click', function (){
+    if(button.innerHTML === 'Ver Mais'){
+        habilidade.style.maxHeight = 'max-content';
+    gradiente.style.background = 'none';
+    button.innerHTML = 'Ver Menos'
+} else{
+    habilidade.style.maxHeight = '50rem';
+    gradiente.style.background = 'linear-gradient(to bottom, rgba(61, 50, 44, 0), #3d322c)';
+    button.innerHTML = 'Ver Mais'
+}
+    
+})
 
 /* typeWriter(fName); */
 
