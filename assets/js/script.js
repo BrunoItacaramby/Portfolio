3abd527020b56fba66d11c99e07c9e08411dc639
BrunoItacaramby <brunobob69@hotmail.com>
2024-02-25 01:31:50 -0300

    Menu Mobile 95% finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,41 @@ var button = document.getElementById('botao-habilidades')
 var habilidade = document.getElementById('habilidades')
 var gradiente = document.getElementById('gradiente')
 meuNome.innerHTML='';
+
+
 var menu1 = document.getElementById('menu-mobile')
 var menu2 = document.getElementById('menu-mobile-content')
 
 menu1.addEventListener('click', function () {
-    // menu2.style.right = '0px'
+    if(document.getElementById('close-menu').checked === false){
+        document.getElementById('close-menu').checked = true
+        menu2.style.transition = 'all 100ms ease-in'
     menu2.style.transform = 'translateX(-45px)'
+    }else{
+        document.getElementById('close-menu').checked = false;
+        menu2.style.transition = 'all 100ms ease-in'
+    menu2.style.transform = 'translateX(0px)'
+    }
+    
+
+})   
+   
 
 
-    // sr.reveal('#menu-mobile-content', {
-    //     // rotate: {x: 0, y: 80, z: 0},
-    //     distance: '50px',
-    //     origin: 'right',
-    //     duration: 500,
-    //     delay: 50
-    // });
-});
+
+// menu1.addEventListener('click', function () {
+//     menu2.style.right = '0px'
+//     menu2.style.transform = 'translateX(-45px)'
+
+
+//     sr.reveal('#menu-mobile-content', {
+//         // rotate: {x: 0, y: 80, z: 0},
+//         distance: '50px',
+//         origin: 'right',
+//         duration: 500,
+//         delay: 50
+//     });
+// });
 
 window.sr = ScrollReveal({ reset: true})
 
